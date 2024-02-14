2ecb4ff2f11fd8f40cec02c61bf984f9d9269fca
Geoff Dutton <g.dutton@gmail.com>
2024-02-13 19:29:28 -0600

    Adding test from that passes in 0.4.14

--- chromadb/test/test_persist_after_delete.py
+++ chromadb/test/test_persist_after_delete.py
@@ -10,24 +10,37 @@ def remove_persist_dir(persist_dir: str) -> None:
 
 
 def test_persist_reload_after_delete() -> None:
-    persist_dir = './test_persist/db/chroma'
+    persist_dir = './fixture_for_test_persist_reload_after_deleted/chroma'
     remove_persist_dir(persist_dir)
+
     chroma_client = chromadb.PersistentClient(path=persist_dir)
 
     collection = chroma_client.create_collection(name="my_collection")
 
     collection.add(
+        embeddings=[[1, 2, 3], [4, 5, 6]],
         documents=["This is a document", "This is another document"],
         metadatas=[{"source": "my_source"}, {"source": "my_source"}],
         ids=["id1", "id2"]
     )
 
     assert os.path.exists(persist_dir), f"Persist directory {persist_dir} does not exist"
+
+    # Remove in the same process
     remove_persist_dir(persist_dir)
 
+    # Reload the client
+    # Roughly trying to follow the langchain Chroma.from_tests(..)
+    ## https://github.com/langchain-ai/langchain/blob/master/libs/community/langchain_community/vectorstores/chroma.py#L682
     chroma_client = chromadb.PersistentClient(path=persist_dir)
 
-    collection = chroma_client.get_collection(name="my_collection")
+    try:
+        chroma_client.get_collection(name="my_collection")
+        assert False, "Collection should not exist"
+    except ValueError:
+        pass
+
+    collection = chroma_client.create_collection(name="my_collection")
 
     collection.add(
         documents=["This is a document", "This is another document"],
